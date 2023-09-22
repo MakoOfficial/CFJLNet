da3477d8354cb6e0f59cd5e86530631f92851633
Mako <112064214+MakoOfficial@users.noreply.github.com>
2023-09-22 16:40:16 +0800

    Update mytraining.py

--- mytraining.py
+++ mytraining.py
@@ -11,18 +11,16 @@ loss_fn = nn.L1Loss(reduction='sum')
 
 # criterion = nn.CrossEntropyLoss(reduction='none')
 if __name__ == '__main__':
-    loss_fn = nn.L1Loss(reduction='sum')
+
     # print(type(image))
     MC = 32
     MF = 64
     beta = 0.95
     Fine_C = torch.zeros((1, MF*256))
     Coarse_C = torch.zeros((1, MC*512))
-    num_hiddens = 128
-    cls_weight = 3
-    genderSize = 32
-    gender = torch.randint(0, 2, (10, 1), dtype=torch.float)
-    net = myKit.get_net(MF=MF, MC=MC, beta=beta, num_hiddens=num_hiddens, genderSize=genderSize)
+    num_hiddens = 256
+    cls_weight = 2
+    net = myKit.get_net(MF, MC, beta, num_hiddens)
     lr = 5e-4
     batch_size = 32
     num_epochs = 50
@@ -37,5 +35,3 @@ if __name__ == '__main__':
     torch.set_default_tensor_type('torch.FloatTensor')
     # myKit.map_fn(net=net, train_dataset=train_set, valid_dataset=val_set, num_epochs=num_epochs, lr=lr, wd=weight_decay, lr_period=lr_period, lr_decay=lr_decay,loss_fn=loss_fn, batch_size=batch_size, model_path="model.pth", record_path="RECORD.csv")
     myKit.map_fn(net, train_set, val_set, num_epochs, lr, weight_decay, lr_period, lr_decay, loss_fn, cls_weight, Fine_C, Coarse_C, batch_size=batch_size, model_path="./model.pth", record_path="./RECORD.csv")
-
-        
